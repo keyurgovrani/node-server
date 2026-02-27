aee7c03502237103da4e52c7b958ae465e9d70ca
Keyur Govrani <keyur.govrani@unifyapps.com>
2026-02-27 17:17:33 +0530

    fix: update log message for server port on startup
    - Changed console log from "Port:" to "Print Port:" for improved clarity in server startup logs.

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ app.post("/api/data", (req, res) => {
 // Start server
 app.listen(PORT, () => {
 	console.log("Environment:", process.env.NODE_ENV);
-	console.log("Port:", PORT);
+	console.log("Print Port:", PORT);
 	console.log(`Server is running on http://localhost:${PORT}`);
 });
